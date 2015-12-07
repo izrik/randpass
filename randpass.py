490853b3d178695558a5da4dcce03cefb24a2cf6
izrik <izrik@izrik.com>
2015-12-07 11:19:46 -0600

    By default, only use letters and numbers. Use symbols if requested.

--- randpass.py
+++ randpass.py
@@ -8,10 +8,13 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--length', type=int, action='store', default=16)
 parser.add_argument('-n', action='store_true')
 parser.add_argument('--count', type=int, action='store', default=1)
+parser.add_argument('--include-symbols', action='store_true')
 
 args = parser.parse_args()
 
-chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789=+'
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+if args.include_symbols:
+    chars += '=+'
 
 for i in xrange(args.count):
     password = ''.join(choice(chars) for _ in xrange(args.length))
