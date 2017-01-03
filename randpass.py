fde8736762956815aece7624ea4313dce3ba505b
izrik <izrik@izrik.com>
2017-01-03 16:46:38 -0600

    Split the script up into a command-line-only part and a re-usable function.

--- randpass.py
+++ randpass.py
@@ -4,26 +4,31 @@ from __future__ import print_function
 from random import choice
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--length', type=int, action='store', default=16)
-parser.add_argument('-n', action='store_true')
-parser.add_argument('--count', type=int, action='store', default=1)
-parser.add_argument('--include-symbols', action='store_true')
-parser.add_argument('--lower', action='store_true')
 
-args = parser.parse_args()
+def generate_password(lowercase=False, include_symbols=False, length=16):
+    chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    if lowercase:
+        chars = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    if include_symbols:
+        chars += '=+'
 
-chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-if args.lower:
-    chars = 'abcdefghijklmnopqrstuvwxyz0123456789'
-if args.include_symbols:
-    chars += '=+'
+    return ''.join(choice(chars) for _ in xrange(length))
 
-for i in xrange(args.count):
-    password = ''.join(choice(chars) for _ in xrange(args.length))
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--length', type=int, action='store', default=16)
+    parser.add_argument('-n', action='store_true')
+    parser.add_argument('--count', type=int, action='store', default=1)
+    parser.add_argument('--include-symbols', action='store_true')
+    parser.add_argument('--lower', action='store_true')
 
-    if args.n:
-        print(password, end='')
-    else:
-        print(password)
+    args = parser.parse_args()
 
+    for i in xrange(args.count):
+        password = generate_password(lowercase=args.lower,
+                                     include_symbols=args.include_symbols,
+                                     length=args.length)
+        if args.n:
+            print(password, end='')
+        else:
+            print(password)
